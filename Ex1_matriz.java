a0184f19f5eaf5367c7d3659615c3dbe7007b09e
QUEILA SOUTO <73967682+Queila-Souto@users.noreply.github.com>
2020-12-06 13:35:20 -0300

    Rename Ex1.java to Ex1_matriz.java

--- Ex1_matriz.java
+++ Ex1_matriz.java
@@ -11,28 +11,28 @@ public class Ex1 {
 	for (int c=0 ; c<tc ; c++) {
 		for (int l=0 ; l<tl ; l++) {
 			System.out.println("Digite num numero inteiro:");
-			matriz1[c][l] = ler.nextInt();	} } // Esse looping est· gerando os valores na matriz
+			matriz1[c][l] = ler.nextInt();	} } // Esse looping est√° gerando os valores na matriz
 	
 	System.out.println("\n___________________________" );
-	System.out.println("\nOs numeros maiores que 10 s„o: \n" );
+	System.out.println("\nOs numeros maiores que 10 s√£o: \n" );
 	
 	for (int c=0 ; c<tc ; c++) {
 		for (int l=0 ; l<tl ; l++) {
 			if (matriz1[c][l] > 10) {
-				System.out.println("PosiÁ„o: ["+c+"] ["+l+"] = " + matriz1[c][l]);
+				System.out.println("Posi√ß√£o: ["+c+"] ["+l+"] = " + matriz1[c][l]);
 				contagem = contagem + 1 ;}
-				} } // Esse looping est· checando os n˙meros maiores que 10 e fazendo a contagem
+				} } // Esse looping est√° checando os n√∫meros maiores que 10 e fazendo a contagem
 	
 	
 	System.out.println("\nQuantidade : " + contagem);
 	System.out.println("___________________________" );
-	System.out.println("\n Aqui est· sua matriz:");
+	System.out.println("\n Aqui est√° sua matriz:");
 	
 	
 	for (int c=0 ; c<tc ; c++) {
 		for (int l=0 ; l<tl ; l++) {
-			System.out.println("PosiÁ„o: ["+c+"] ["+l+"] = " + matriz1[c][l]);
-			}} // Esse looping È para exibir a toda a matriz
+			System.out.println("Posi√ß√£o: ["+c+"] ["+l+"] = " + matriz1[c][l]);
+			}} // Esse looping √© para exibir a toda a matriz
 		}
 	}
 }
