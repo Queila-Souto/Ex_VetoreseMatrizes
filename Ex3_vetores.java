ca98101beee7cdee7d26b91248ccd586da99ea9e
QUEILA SOUTO <73967682+Queila-Souto@users.noreply.github.com>
2020-12-06 13:13:10 -0300

    Update and rename Ex3.java to Ex3_vetores.java

--- Ex3_vetores.java
+++ Ex3_vetores.java
@@ -1,3 +1,6 @@
+/*Ler um conjunto de numeros reais, armazenando-o em vetor e calcular o quadrado das 
+componentes deste vetor, armazenando o resultado em outro vetor. Os conjuntos tem
+10 elementos cada. Imprimir todos os conjuntos.*/
 import java.util.Scanner;
 
 public class Ex3 {
