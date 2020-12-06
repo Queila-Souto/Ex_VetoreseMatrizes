9ac85759aa12e3873ba3bc61b4f0ca6757c2bc72
QUEILA SOUTO <73967682+Queila-Souto@users.noreply.github.com>
2020-12-06 13:14:58 -0300

    Rename Ex6.java to Ex6_vetores.java

--- Ex6_vetores.java
+++ Ex6_vetores.java
@@ -1,4 +1,4 @@
-// programa para pedir um vetor de 10 posições e exibir o menor e o maior valor
+// programa para pedir um vetor de 10 posiÃ§Ãµes e exibir o menor e o maior valor
 
 import java.util.Scanner;
 public class Ex6 {
@@ -7,24 +7,24 @@ public class Ex6 {
 		int t = 5; // Fiz dessa forma para facilitar a troca de valores referente ao tamanho do vetor
 		int vetor1[]= new int[t];
 		int maior = Integer.MIN_VALUE; // valor minimo atribuido a variavel
-		int menor = Integer.MAX_VALUE; // valor máximo atribuido a variavel
+		int menor = Integer.MAX_VALUE; // valor mÃ¡ximo atribuido a variavel
 		
 		for (int i=0; i<t ; i++) {
 			System.out.println("Digite um numero");
 			vetor1[i] = ler.nextInt();
-		} // definição do vetor
+		} // definiÃ§Ã£o do vetor
 		
 		for (int i=0; i<t ; i++) { 		
 		maior = (maior>=vetor1[i]) ? maior: vetor1[i]; 
 		menor = (menor<=vetor1[i]) ? menor: vetor1[i]; 
 			}
 		// Acima optei por uma forma alternativa de if. 
-		//Pergunto se a variável é maior ou igual ao conteudo do vetor na posição atual. 
-		//Se sim eu mantenho o valor da variável, se nao o valor é trocado pelo conteudo da posição atual do vetor.
+		//Pergunto se a variÃ¡vel Ã© maior ou igual ao conteudo do vetor na posiÃ§Ã£o atual. 
+		//Se sim eu mantenho o valor da variÃ¡vel, se nao o valor Ã© trocado pelo conteudo da posiÃ§Ã£o atual do vetor.
 		
 		System.out.println("======================== ");
-		System.out.println("O maior valor é " + maior);
-		System.out.println("O menor valor é " + menor);
+		System.out.println("O maior valor Ã© " + maior);
+		System.out.println("O menor valor Ã© " + menor);
 		System.out.println("======================== ");
 
 		}
